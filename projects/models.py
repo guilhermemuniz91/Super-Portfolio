b5649cbce848caa9c89fe4897839e52a8364b523
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-04-10 11:57:23 -0300

    req2 feito

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    github = models.URLField(max_length=500, null=False)
+    linkedin = models.URLField(max_length=500, null=False)
+    bio = models.TextField(null=False)
+
+    def __str__(self):
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from projects.models import Profile
+from projects.serializers import ProfileSerializer
 
 # Create your views here.
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            profile = self.get_object()
+            context = {
+                "profile": profile,
+                "projects": profile.projects.all(),
+                "certificates": profile.certificates.all(),
+            }
+            return render(request, "profile_detail.html", context)
+        return super().retrieve(request, *args, **kwargs)
